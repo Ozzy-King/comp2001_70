7de87f25b36e7130ace59f8f3aca3636451917e5
Osbourne L A Clark <osbourne.clark14@gmail.com>
2023-12-19 21:06:27 +0000

    changed to swagger, added database connction class, get public user data api added

---
+++ WebApp2new/WebApp2/Controllers/profileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace WebApp2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class profileController : ControllerBase
+    {
+        // GET: proflie
+        [HttpGet(Name = "GetUser")]
+        public string getUser(String email) {
+            string connectionString = "Server=dist-6-505.uopnet.plymouth.ac.uk;Database=COMP2001_OClark;User Id=OClark;Password=GgyC627+;";
+            string commandString = "SELECT * FROM cw2.publicUserData where email = '" + email + "'";
+            DatabaseConnectionClass databaseConnection = new DatabaseConnectionClass();
+            databaseConnection.connect(connectionString);
+            return databaseConnection.executeCommand(commandString);
+
+
+            //using (SqlConnection con = new SqlConnection(connectionString)) {
+            //    con.Open();
+            //    string commandString = "SELECT * FROM cw2.publicUserData where email = '"+email+"'";
+            //    SqlCommand command = new SqlCommand(commandString, con);
+            //    using (SqlDataReader reader = command.ExecuteReader()) {
+            //        while (reader.Read()) {
+            //            return reader.GetValue(1).ToString() + reader.GetValue(2).ToString();
+            //        }
+            //    }
+            //    return "";
+            //}
+        }
+
+    }
+}
---
+++ WebApp2new/WebApp2/DatabaseConnectionClass.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.JSInterop;
+using Newtonsoft.Json.Linq;
+using System.Data.SqlClient;
+
+namespace WebApp2
+{
+    
+
+    public class DatabaseConnectionClass
+    {
+        SqlConnection? _databaseConnection;
+        public bool connect(string server, string database, string username, string password) {
+            string connectionString = "Server="+server+";Database="+database+";User Id="+username+";Password="+password+";";
+            _databaseConnection = new SqlConnection(connectionString);
+            _databaseConnection.Open();
+            return false;
+        }
+        public bool connect(string connectionString) {
+            _databaseConnection = new SqlConnection(connectionString);
+            _databaseConnection.Open();
+            return false;
+        }
+
+        //takes a command and returns a json string
+        public string executeCommand(string Command) {
+            string output = "[";
+            string[] getnames;
+            bool checkflipper = false;
+            if (_databaseConnection == null) { return output + "]"; }
+            SqlCommand command = new SqlCommand(Command, _databaseConnection);
+            SqlDataReader reader = command.ExecuteReader();
+            
+            //gets the names of the colums
+            getnames = new string[reader.FieldCount];
+            for (int i = 0; i < getnames.Length; i++) {
+                getnames[i] = reader.GetName(i);
+            }
+
+            //fills in json string from return type
+            while (reader.Read()) {
+
+                //if false, change flipper to true so it will add a comma only after the first loop.
+                if (!checkflipper) { checkflipper = true; }
+                else {
+                    output += ",";
+                }
+                output += "{";
+                for (int i = 0; i < getnames.Length; i++) {
+                    output += "\"" + getnames[i] + "\":\"" + reader.GetValue(i).ToString()+"\"";
+                    if (i < getnames.Length-1) {
+                        output += ",";
+                    }
+                }
+                output += "}";
+            }
+            return output + "]" ;
+        }
+
+        ~DatabaseConnectionClass() {
+            if (_databaseConnection == null) { return; }
+            _databaseConnection.Close();
+        }
+
+    }
+}
